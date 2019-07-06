9dc232ea0cba998880b92b54a7e9b736a2c5a944
Kumar Gourav <kgouravcode@gmail.com>
2019-07-06 10:54:20 +0530

    Project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/kg/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kg.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
